eb0baa241a8494378be9ae04d061f82c20187fe4
vishal2013 <vishal.rana0008@gmail.com>
2021-03-22 09:56:10 +0530

    Update AddContactInServer.java

--- AddContactInServer.java
+++ AddContactInServer.java
@@ -1,16 +1,14 @@
 package com.example.ablejobssynccontacts;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -27,11 +25,15 @@ public class AddContactInServer extends AppCompatActivity {
     private EditText phoneEditText;
     private Button addContactButton;
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference contactsServerDBRef = db.collection("contactsDB");
+    private static final String KEY_TIME_STAMP = "Time_stamp";
+    private static final String KEY_NAME = "Name";
+    private static final String KEY_PHONE = "Phone";
 
-    private SharedPreferences sharedPref;
-    private int lastWrittenContactId;
+    private static final String CONTACTS_DB = "contactsDB";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference contactsServerDBRef = db.collection(CONTACTS_DB);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,42 +44,36 @@ public class AddContactInServer extends AppCompatActivity {
         nameEditText = findViewById(R.id.edit_text_name);
         phoneEditText = findViewById(R.id.edit_text_phone);
         addContactButton = findViewById(R.id.add_contact_button);
-        sharedPref = getSharedPreferences("com.example.ablejobssynccontacts.lastWrittenContactId"
-                , Context.MODE_PRIVATE);
 
         addContactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lastWrittenContactId = sharedPref.getInt("last_written_contact_id", 0);
+                long timeStamp = System.currentTimeMillis();
                 String name = nameEditText.getText().toString();
                 String phone = phoneEditText.getText().toString();
 
                 if (name.isEmpty() || phone.isEmpty()) {
-                    Toast.makeText(getApplicationContext(),
-                            "Please enter valid data", Toast.LENGTH_SHORT).show();
+                    showToast("Please enter valid data");
                     return;
                 }
 
                 Map<String, Object> mp = new HashMap<>();
-                mp.put("Contact_id", lastWrittenContactId + 1);
-                mp.put("Name", name);
-                mp.put("Phone", phone);
+                mp.put(KEY_TIME_STAMP, timeStamp);
+                mp.put(KEY_NAME, name);
+                mp.put(KEY_PHONE, phone);
 
                 contactsServerDBRef.document().set(mp)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(getApplicationContext(),
-                                "Contact added successfully on Server DB", Toast.LENGTH_SHORT).show();
-                        sharedPref.edit().putInt("last_written_contact_id", ++lastWrittenContactId).apply();
-                        nameEditText.setText("");
-                        phoneEditText.setText("");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                showToast("Contact added successfully on Server DB");
+                                nameEditText.setText("");
+                                phoneEditText.setText("");
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(),
-                                "Oops !! something went wrong ....", Toast.LENGTH_LONG).show();
+                        showToast("Oops !! something went wrong ....");
                         Log.w(TAG, "Error writing document", e);
                     }
                 });
@@ -85,4 +81,9 @@ public class AddContactInServer extends AppCompatActivity {
             }
         });
     }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
 }
