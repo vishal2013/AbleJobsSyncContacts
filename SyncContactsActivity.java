beec98f1093c5254a39e45f61261a3c3cefcfba7
vishal2013 <vishal.rana0008@gmail.com>
2021-03-22 09:55:03 +0530

    Update SyncContactsActivity.java

--- SyncContactsActivity.java
+++ SyncContactsActivity.java
@@ -1,16 +1,9 @@
 package com.example.ablejobssynccontacts;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,31 +14,45 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SyncContactsActivity extends AppCompatActivity {
     private static final String TAG = "SyncContactsActivity";
 
-    private static final String[] permissions = {Manifest.permission.WRITE_CONTACTS,
-            Manifest.permission.READ_CONTACTS};
-    private static final int CONTACTS_READ_WRITE_PERMISSIONS_CODE = 100;
+    private static final String[] permissions = {Manifest.permission.WRITE_CONTACTS};
+
+    private static final int CONTACTS_WRITE_PERMISSIONS_CODE = 100;
+    private static final String KEY_TIME_STAMP = "Time_stamp";
+    private static final String KEY_NAME = "Name";
+    private static final String KEY_PHONE = "Phone";
+    private static final String KEY_LAST_SYNC_TIME = "Last_sync_time";
+
+    private static final String DOC_LAST_SYNCED_INFO = "LastSyncedInfo";
+    private static final String CONTACTS_DB = "contactsDB";
 
     private Button addContactsInServerDBButton;
     private Button syncContactsButton;
     private ProgressBar progressBar;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference contactsServerDBRef = db.collection("contactsDB");
+    private CollectionReference contactsServerDBRef = db.collection(CONTACTS_DB);
 
-    private SharedPreferences sharedPref;
-    private int lastSyncedContactId;
+    private long lastSyncTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,61 +60,37 @@ public class SyncContactsActivity extends AppCompatActivity {
         setContentView(R.layout.sync_contacts_activity_layout);
         setTitle("Able Sync Contacts");
 
-        checkPermission(permissions, CONTACTS_READ_WRITE_PERMISSIONS_CODE);
+        checkPermission(permissions, CONTACTS_WRITE_PERMISSIONS_CODE);
 
         addContactsInServerDBButton = findViewById(R.id.add_server_db_contacts_button);
         syncContactsButton = findViewById(R.id.sync_contacts_button);
         progressBar = findViewById(R.id.progress_bar);
-        progressBar.setVisibility(View.GONE);
-
-        sharedPref = getSharedPreferences("com.example.ablejobssynccontacts.lastSyncedContactId"
-                , Context.MODE_PRIVATE);
+        setProgBarVisibility(false);
 
         createButtonListeners();
     }
 
     private void createButtonListeners() {
+
         syncContactsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lastSyncedContactId = sharedPref.getInt("last_synced_contact_id", 0);
 
-                contactsServerDBRef.whereGreaterThan("Contact_id", lastSyncedContactId)
-                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                contactsServerDBRef.document(DOC_LAST_SYNCED_INFO).get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
-                            int totalCnt = task.getResult().size();
-
-                            if (totalCnt == 0) {
-                                Toast.makeText(getApplicationContext(),
-                                        "Contacts already synced ...", Toast.LENGTH_SHORT).show();
-                                return;
+                            DocumentSnapshot doc = task.getResult();
+                            if (doc.exists()) {
+                                lastSyncTime = doc.getLong(KEY_LAST_SYNC_TIME);
+                            } else {
+                                lastSyncTime = 0;
                             }
 
-                            progressBar.setProgress(0);
-                            progressBar.setVisibility(View.VISIBLE);
-                            progressBar.setMax(totalCnt);
+                            fetchNewContactsAndStoreInDB();
 
-                            int insertCnt = 0;
-                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                                String displayName = document.getString("Name");
-                                String phoneNumber = document.getString("Phone");
-
-                                if (insertContactInLocalDB(displayName, phoneNumber)) {
-                                    lastSyncedContactId++;
-                                    progressBar.setProgress(++insertCnt);
-                                }
-                            }
-                        } else {
-                            Toast.makeText(getApplicationContext(),
-                                    "Oops !!! Error getting contacts ...", Toast.LENGTH_LONG).show();
-                            Log.d(TAG, "Oops !!! Error getting contacts ...", task.getException());
                         }
-
-                        sharedPref.edit().putInt("last_synced_contact_id", lastSyncedContactId).apply();
-                        progressBar.setVisibility(View.GONE);
                     }
                 });
             }
@@ -122,16 +105,75 @@ public class SyncContactsActivity extends AppCompatActivity {
         });
     }
 
+    private void fetchNewContactsAndStoreInDB() {
+        contactsServerDBRef.whereGreaterThan(KEY_TIME_STAMP, lastSyncTime)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if (task.isSuccessful()) {
+                    int totalCnt = task.getResult().size();
+
+                    if (totalCnt == 0) {
+                        showToast("Contacts already synced ...");
+                        return;
+                    }
+
+                    initProgressBar(totalCnt);
+
+                    long maxTimeStamp = 0;
+                    int insertCnt = 0;
+                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                        long timeStamp = document.getLong(KEY_TIME_STAMP);
+                        String displayName = document.getString(KEY_NAME);
+                        String phoneNumber = document.getString(KEY_PHONE);
+
+                        if (insertContactInLocalDB(displayName, phoneNumber)) {
+                            if (timeStamp > maxTimeStamp) {
+                                maxTimeStamp = timeStamp;
+                            }
+                            progressBar.setProgress(++insertCnt);
+                        }
+                    }
+
+                    showToast("New contacts synced with local contacts db.");
+
+                    lastSyncTime = maxTimeStamp;
+                    Map<String, Object> mp = new HashMap<>();
+                    mp.put(KEY_LAST_SYNC_TIME, lastSyncTime);
+                    contactsServerDBRef.document(DOC_LAST_SYNCED_INFO).set(mp);
+                } else {
+                    showToast("Oops !!! Error getting contacts ...");
+                    Log.d(TAG, "Oops !!! Error getting contacts ...", task.getException());
+                }
+
+                setProgBarVisibility(false);
+            }
+        });
+    }
+
     boolean insertContactInLocalDB(String displayName, String phoneNumber) {
         Uri addContactsUri = ContactsContract.Data.CONTENT_URI;
         long rowContactId = getRawContactId();
 
         if (insertContactDisplayName(addContactsUri, rowContactId, displayName) != null
                 && insertContactPhoneNumber(addContactsUri, rowContactId, phoneNumber) != null) {
-            Toast.makeText(getApplicationContext(),
-                    "New contacts synced with local contacts db.", Toast.LENGTH_SHORT).show();
             return true;
         } else return false;
+    }
+
+    private void initProgressBar(int max) {
+        progressBar.setProgress(0);
+        progressBar.setMax(max);
+        setProgBarVisibility(true);
+    }
+
+    private void setProgBarVisibility(boolean visibility) {
+        progressBar.setVisibility(visibility ? View.VISIBLE : View.GONE);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
     private long getRawContactId() {
@@ -177,5 +219,15 @@ public class SyncContactsActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == CONTACTS_WRITE_PERMISSIONS_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                return;
+            } else {
+                finish();
+            }
+        }
+    }
 
 }
